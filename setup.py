5022d7448294c18ba276888e5776bb639e6f2e21
Alberto Rubio <alberto.rubio@beigar.com>
2022-03-04 13:00:26 +0100

    Comment LOG

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from distutils.core import setup
 
-version = '1.8.0'
+version = '1.8.1'
 
 with open('README.markdown') as readme:
     long_description = readme.read()
 
 setup(
-    name = 'dicttoxml',
-    version = version,
-    description = 'Converts a Python dictionary or other native data type into a valid XML string. ',
-    long_description = long_description,
-    author = 'Ryan McGreal',
-    author_email = 'ryan@quandyfactory.com',
-    license = 'LICENCE.txt',
-    url = 'https://github.com/quandyfactory/dicttoxml',
-    py_modules = ['dicttoxml'],
-    download_url = 'https://pypi.python.org/packages/source/d/dicttoxml/dicttoxml-%s.tar.gz?raw=true' % (version),
+    name='dicttoxml',
+    version=version,
+    description='Converts a Python dictionary or other native data type into a valid XML string. ',
+    long_description=long_description,
+    author='Alberto Rubio',
+    author_email='alberto.rubio@beigar.com',
+    license='LICENCE.txt',
+    url='https://github.com/arh-beigar/dicttoxml',
+    py_modules=['dicttoxml'],
+    download_url='https://pypi.python.org/packages/source/d/dicttoxml/dicttoxml-%s.tar.gz?raw=true' % (version),
     platforms='Cross-platform',
     classifiers=[
       'Programming Language :: Python',
